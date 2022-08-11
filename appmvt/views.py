d132deb7c5e00ee2002d64d2f1ca31e4ddfe4244
jeileenl <110411529+jeileenl@users.noreply.github.com>
2022-08-10 22:57:50 -0300

    Add files via upload

--- appmvt/views.py
+++ appmvt/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from appmvt.models import Familiares
 
-def lista_familiares(request):
-    familia = Familiares.objects.all ()
-    return HttpResponse (familia[3].edad)
+
+def inicio(request):
+    return HttpResponse ("inicio")
+
+def post(request):
+    return HttpResponse ("post")
+
+def vista_editor(request):
+    return HttpResponse ("editor")
+
+def about(request):
+    return HttpResponse("about")
+
+def contacto(request):
+    return HttpResponse("contacto")
+
+def mensaje (request):
+    return HttpResponse ("mensaje")
     
